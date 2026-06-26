6ba91d63175980a0a034f1ee2b3b13374c662bb9
Amit Das <112541611+AmitDas4321@users.noreply.github.com>
2026-06-26 14:21:27 +0530

    Update HLS-Converter.ps1

--- HLS-Converter.ps1
+++ HLS-Converter.ps1
@@ -17,7 +17,6 @@ $form.Controls.Add($fileBox)
 
 
 
-
 $browse = New-Object System.Windows.Forms.Button
 $browse.Text = "Browse"
 $browse.Location = New-Object System.Drawing.Point(400,40)
@@ -26,16 +25,16 @@ $browse.Size = New-Object System.Drawing.Size(100,25)
 
 $browse.Add_Click({
 
-    $dialog = New-Object System.Windows.Forms.OpenFileDialog
+$dialog = New-Object System.Windows.Forms.OpenFileDialog
 
-    $dialog.Filter = "Video Files|*.mkv;*.mp4;*.mov;*.avi;*.webm"
+$dialog.Filter = "Video Files|*.mkv;*.mp4;*.mov;*.avi;*.webm"
 
 
-    if($dialog.ShowDialog() -eq "OK"){
+if($dialog.ShowDialog() -eq "OK"){
 
-        $fileBox.Text = $dialog.FileName
+$fileBox.Text = $dialog.FileName
 
-    }
+}
 
 })
 
@@ -44,56 +43,52 @@ $form.Controls.Add($browse)
 
 
 
-
 $label = New-Object System.Windows.Forms.Label
-$label.Text = "Output Folder Name"
-$label.Location = New-Object System.Drawing.Point(20,90)
-$label.Size = New-Object System.Drawing.Size(200,25)
+$label.Text="Output Folder Name"
+$label.Location=New-Object System.Drawing.Point(20,90)
 $form.Controls.Add($label)
 
 
 
 $folderBox = New-Object System.Windows.Forms.TextBox
-$folderBox.Location = New-Object System.Drawing.Point(20,120)
-$folderBox.Size = New-Object System.Drawing.Size(360,25)
+$folderBox.Location=New-Object System.Drawing.Point(20,120)
+$folderBox.Size=New-Object System.Drawing.Size(360,25)
 $form.Controls.Add($folderBox)
 
 
 
 
 $start = New-Object System.Windows.Forms.Button
-$start.Text = "Start Convert"
-$start.Location = New-Object System.Drawing.Point(20,180)
-$start.Size = New-Object System.Drawing.Size(120,35)
+$start.Text="Start Convert"
+$start.Location=New-Object System.Drawing.Point(20,180)
+$start.Size=New-Object System.Drawing.Size(120,35)
 
 
 
 $start.Add_Click({
 
 
-$inputFile = $fileBox.Text
+$inputFile=$fileBox.Text
 
 
 if(!$inputFile){
 
-    return
+return
 
 }
 
 
 
-# Folder name
-
 if($folderBox.Text){
 
-    $folderName = $folderBox.Text
+$folderName=$folderBox.Text
 
 }
 else{
 
-    $folderName = [System.IO.Path]::GetFileNameWithoutExtension($inputFile)
+$folderName=[System.IO.Path]::GetFileNameWithoutExtension($inputFile)
 
-    $folderName = $folderName -replace '[^a-zA-Z0-9\-]','-'
+$folderName=$folderName -replace '[^a-zA-Z0-9\-]','-'
 
 }
 
@@ -102,67 +97,159 @@ else{
 $basePath = Split-Path $inputFile
 
 
-$outputFolder = Join-Path $basePath $folderName
-
-
-$segmentFolder = Join-Path $outputFolder "segments"
+$out = Join-Path $basePath $folderName
 
 
 
-New-Item -ItemType Directory -Force -Path $segmentFolder | Out-Null
+New-Item -ItemType Directory -Force $out | Out-Null
 
 
 
-$outputFile = Join-Path $outputFolder "output.m3u8"
+$form.Dispose()
 
-
-$segmentFile = Join-Path $segmentFolder "segment_%05d.ts"
-
-
-
-
-$form.Hide()
-
-$form.Close()
+Start-Sleep -Milliseconds 500
 
 
 
-
-ffmpeg -y `
--i "$inputFile" `
--c:v libx264 `
--preset medium `
--profile:v high `
--level 4.0 `
--pix_fmt yuv420p `
--c:a aac `
--b:a 128k `
--hls_time 6 `
--hls_playlist_type vod `
--hls_flags independent_segments `
--hls_base_url "segments/" `
--hls_segment_type mpegts `
--hls_start_number_source 0 `
--hls_segment_filename "$segmentFile" `
--f hls `
-"$outputFile"
+Write-Host ""
+Write-Host "======================"
+Write-Host "HLS CONVERSION START"
+Write-Host $out
+Write-Host "======================"
+Write-Host ""
 
 
 
 
-while($true){
 
-    Start-Sleep -Seconds 9999
+$args = @(
 
-}
+"-y",
+
+"-i",
+$inputFile,
+
+
+"-filter_complex",
+"[0:v]split=5[v1][v2][v3][v4][v5];[v1]scale=1920:1080[v1080];[v2]scale=1280:720[v720];[v3]scale=854:480[v480];[v4]scale=640:360[v360];[v5]scale=426:240[v240]",
+
+
+
+"-map","[v1080]",
+"-map","0:a",
+
+"-map","[v720]",
+"-map","0:a",
+
+"-map","[v480]",
+"-map","0:a",
+
+"-map","[v360]",
+"-map","0:a",
+
+"-map","[v240]",
+"-map","0:a",
+
+
+
+"-c:v",
+"libx264",
+
+"-preset",
+"medium",
+
+"-profile:v",
+"main",
+
+"-pix_fmt",
+"yuv420p",
+
+
+
+"-b:v:0",
+"5000k",
+
+"-b:v:1",
+"2500k",
+
+"-b:v:2",
+"1200k",
+
+"-b:v:3",
+"700k",
+
+"-b:v:4",
+"400k",
+
+
+
+"-c:a",
+"aac",
+
+"-b:a",
+"128k",
+
+
+
+"-f",
+"hls",
+
+"-hls_time",
+"6",
+
+"-hls_playlist_type",
+"vod",
+
+"-hls_flags",
+"independent_segments",
+
+
+
+"-master_pl_name",
+"master.m3u8",
+
+
+
+"-var_stream_map",
+"v:0,a:0,name:1080p v:1,a:1,name:720p v:2,a:2,name:480p v:3,a:3,name:360p v:4,a:4,name:240p",
+
+
+
+"-hls_segment_filename",
+"$out\%v\segment_%05d.ts",
+
+
+
+"$out\%v\playlist.m3u8"
+
+)
+
+
+
+& ffmpeg @args
+
+
+
+
+
+Write-Host ""
+Write-Host "======================"
+Write-Host "HLS DONE"
+Write-Host "$out\master.m3u8"
+Write-Host "======================"
+Write-Host ""
+
+
+
+Read-Host "Press ENTER to close terminal"
 
 
 
 })
 
 
-$form.Controls.Add($start)
 
+$form.Controls.Add($start)
 
 
 
